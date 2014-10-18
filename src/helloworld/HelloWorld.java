9d5a5fcdb2343d658b7f2fb12b5b265977f5fd01
Firstname Lastname <your_email@youremail.com>
2014-10-18 23:34:41 +0900

    init

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,21 @@
+/*
+ Hello World
+ 2014/10/18 M.Wada
+ */
+package helloworld;
+
+/**
+ *
+ * @author wadamaki
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.print("HelloWorld");
+    }
+    
+}
